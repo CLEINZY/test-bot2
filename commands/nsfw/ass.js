bdba3a110fd06217b764a10228a33c87f7e07508
VIXY Store <79237447+CLEINZY@users.noreply.github.com>
2022-07-14 05:45:22 +0000

    Initial commit

---
+++ commands/nsfw/ass.js
@@ -0,0 +1,22 @@
+const request = require('node-superfetch');
+const Color = "RANDOM";
+const Discord = require("discord.js")
+const Random = require("image-cord");
+const fetch = require("node-fetch")
+
+
+module.exports = {
+  name: "ass",
+  description: "Anime",
+
+  run: async (client, message, args) => {
+
+    // const Pussy = await Random.Pussy();
+    //     return message.channel.send(Pussy);
+
+    //    const embed = new Discord.MessageEmbed()
+    // .setImage(fetch(`https://diwasatreya.tech/ass`))
+
+      message.channel.send(`https://diwasatreya.tech/ass`);
+   
+}};
